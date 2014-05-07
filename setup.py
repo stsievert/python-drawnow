31fdb3b7367bafe3960d8e47fee9ecf0e7cbebaa
scottsievert <sieve121@umn.edu>
2014-05-07 13:15:26 -0500

    After pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
-
-setup(name='drawnow',
-        author='Scott Sievert',
-        author_email='sieve121@umn.edu',
-        url='https://github.com/scottsievert/python-drawnow',
-        version='0.1',
-        py_modules=['drawnow'],
-        )
+setup(
+  name         = 'drawnow',
+  packages     = ['drawnow'], # this must be the same as the name above
+  version      = '0.2',
+  description  = 'MATLAB-like drawnow',
+  author       = 'Scott Sievert',
+  author_email = 'sieve121@umn.edu',
+  url          = 'https://github.com/scottsievert/python-drawnow',
+  download_url = 'https://github.com/scottsievert/python-drawnow/archive/master.zip',
+  keywords     = ['figure', 'plotting', 'visualization', 'matlab'],
+  classifiers  = [],
+)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 from __future__ import division
 from pylab import zeros, arange, figure, ion, colorbar, imshow
-from drawnow import drawnow
+from drawnow import refresh
 
 def draw_fig_real():
     imshow(z, interpolation='nearest')
@@ -13,5 +13,5 @@ z = zeros((N,N))
 ion()
 figure()
 for i in arange(4*N):
-    z.flat[i] = 1
-    drawnow(draw_fig_real, show_once=False, confirm=False)
+    z.flat[i] = i
+    refresh(draw_fig_real, show_once=False, confirm=False)
