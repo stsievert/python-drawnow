e9ece274d952fc8a97403e97c8abd51eabc2f5c3
scottsievert <sieve121@umn.edu>
2014-05-07 12:48:27 -0500

    Before pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 
 setup(name='drawnow',
-      version='0.1',
-      py_modules=['drawnow'],
-      )
+        author='Scott Sievert',
+        author_email='sieve121@umn.edu',
+        url='https://github.com/scottsievert/python-drawnow',
+        version='0.1',
+        py_modules=['drawnow'],
+        )
